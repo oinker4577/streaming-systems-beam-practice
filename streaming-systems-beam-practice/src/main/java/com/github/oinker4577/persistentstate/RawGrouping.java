fe4f04f087f1043318b4812d65838112f997f845
KY Fang <fanggot@gmail.com>
2021-01-30 21:01:26 -0800

    add line parsing transform

--- streaming-systems-beam-practice/src/main/java/com/github/oinker4577/persistentstate/RawGrouping.java
+++ streaming-systems-beam-practice/src/main/java/com/github/oinker4577/persistentstate/RawGrouping.java
@@ -5,7 +5,10 @@ import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,15 +17,36 @@ import java.net.URL;
 public class RawGrouping {
   private static final Logger logger = LoggerFactory.getLogger(RawGrouping.class);
 
+  static class ExtractTeamScore extends DoFn<String, KV<String, Long>> {
+    @ProcessElement
+    public void processElement(@Element String element,
+                               OutputReceiver<KV<String, Long>> receiver) {
+      String[] record = element.split(",", -1);
+      receiver.output(KV.of(record[1], Long.valueOf(record[2])));
+    }
+  }
+
+  public static class ParseScoreRecord
+      extends PTransform<PCollection<String>, PCollection<KV<String, Long>>> {
+    @Override
+    public PCollection<KV<String, Long>> expand(PCollection<String> lines) {
+
+      return lines.apply(ParDo.of(new ExtractTeamScore()));
+    }
+  }
+
   public static void main(String[] args) {
     URL inputPath = RawGrouping.class.getClassLoader().getResource("userscore.csv");
     PipelineOptions options = PipelineOptionsFactory.create();
     Pipeline pipeline = Pipeline.create(options);
     pipeline.apply(TextIO.read().from(inputPath.getPath()))
-        .apply(ParDo.of(new DoFn<String, Void>() {
+        .apply(new ParseScoreRecord())
+        .apply(ParDo.of(new DoFn<KV<String, Long>, Void>() {
           @ProcessElement
           public void processElement(ProcessContext c)  {
-            logger.info(c.element());
+            logger.info("key: {}, value: {}",
+                c.element().getKey(),
+                c.element().getValue());
           }
         }));
     pipeline.run();
